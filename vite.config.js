205a8424bac1b6168e836048db0fbc78b35a77b0
anandaAananda <anandanicholaaa@gmail.com>
2022-08-15 13:03:46 +0700

    first and complete

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel([
+//             'resources/css/app.css',
+//             'resources/js/app.js',
+//         ]),
+//     ],
+// });
+
+export default ({ command }) => ({
+    base: command === 'serve' ? '' : '/build/',
+    publicDir: 'fake_dir_so_nothing_gets_copied',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
+});
